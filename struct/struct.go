fa321ee4f156d3afe4e9ef3a1fbe3f2507f833d4
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2022-01-06 14:31:16 +0700

    Modify: Adding some code

--- struct/struct.go
+++ struct/struct.go
@@ -8,6 +8,17 @@ type Anggota struct {
 	kelas                    int
 }
 
+// * di golang kita juga bisa membuat struct seolah olah memiliki sebuah function / mehotd
+// * parameter anggota tadi bisa dinamai dengan yang lainya misalkan a, b, dll
+func (anggota Anggota) sayHelloFromKetu(name string) {
+	fmt.Println("Hallo", name, "nama saya", anggota.ketua)
+}
+
+// * contoh lain
+func (a Anggota) sayHuuuu() {
+	fmt.Println("Huuuuuu form", a.ketua)
+}
+
 func main() {
 	// * di dalam go kita bisa menggunakan custom type dengan menggunakan struct
 	ipa := Anggota{
@@ -18,4 +29,10 @@ func main() {
 	}
 	// * untuk mengakses variabel di dalam struct kita bisa menggunakan seperti di bawah ini
 	fmt.Println(ipa.ketua)
+
+	fmt.Println("==================")
+
+	// * nah disini kita bisa mengakses sayHelloFromKetu dengan menggunakan varuabel yang sudah di assign menggunakan struct tadi
+	ipa.sayHelloFromKetu("ivan")
+	ipa.sayHuuuu()
 }
